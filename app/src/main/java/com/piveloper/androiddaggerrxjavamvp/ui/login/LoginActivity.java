e799ff988847c68f129e4f874278d8b28cc43618
piveloper <piveloper@gmail.com>
2017-09-18 16:03:52 +0200

    Generated empty login activity

---
+++ app/src/main/java/com/piveloper/androiddaggerrxjavamvp/ui/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.piveloper.androiddaggerrxjavamvp.ui.login;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.piveloper.androiddaggerrxjavamvp.R;
+import com.piveloper.androiddaggerrxjavamvp.ui.base.BaseActivity;
+
+public class LoginActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
