333a78b245beabb9abc7829e107a681c671e46fa
piveloper <piveloper@gmail.com>
2017-09-18 14:40:28 +0200

    Created ApplicationComponent through Dagger

--- app/src/main/java/com/piveloper/androiddaggerrxjavamvp/AppController.java
+++ app/src/main/java/com/piveloper/androiddaggerrxjavamvp/AppController.java
@@ -2,20 +2,28 @@ package com.piveloper.androiddaggerrxjavamvp;
 
 import android.app.Application;
 
+import com.piveloper.androiddaggerrxjavamvp.data.DataManager;
 import com.piveloper.androiddaggerrxjavamvp.di.components.ApplicationComponent;
+import com.piveloper.androiddaggerrxjavamvp.di.components.DaggerApplicationComponent;
+import com.piveloper.androiddaggerrxjavamvp.di.modules.ApplicationModule;
+
+import javax.inject.Inject;
 
 /**
  * Created by P.Mohr on 18.09.2017.
  */
 
 public class AppController extends Application {
-    public static ApplicationComponent sApplicationComponent;
+    @Inject
+    DataManager mDataManager;
 
-    ApplicationComponent getApplicationComponent(){
-        return sApplicationComponent;
-    }
+    private ApplicationComponent mApplicationComponent;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        mApplicationComponent = DaggerApplicationComponent.builder()
+                .applicationModule(new ApplicationModule(this)).build();
+        mApplicationComponent.inject(this);
     }
 }
